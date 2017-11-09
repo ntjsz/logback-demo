7b8f9074be9e537ad461c485613a0345e314ca81
hht <lakehaitao@gmail.com>
2017-11-09 11:15:25 +0800

    hi

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hht on 2017.11.9.
+ */
+public class Main {
+    final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("first hi");
+    }
+}
